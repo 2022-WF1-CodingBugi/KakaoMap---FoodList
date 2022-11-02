f1a4d6957e37d3c3104298b41887e050d7109136
zzawang <hobin0116@naver.com>
2022-11-03 08:56:43 +0900

    foodList

--- src/Components/FoodList/FoodList.js
+++ src/Components/FoodList/FoodList.js
@@ -4,16 +4,13 @@ import { restaurant } from '../../Data/restaurant';
 
 
 function FoodList() {
-
-    const [clicks, changeClicks] = useState(false);
-    const onClick = () => {
-        changeClicks(!clicks);
-    };
+    const [name, compareName] = useState("");
+    const [click, changeClicks] = useState(false);
     
     const list = restaurant.map((restaurant, i) =>
-    <div className = 'Food-List-Item-div' key = {i}><p className='Food-List-Item' key = {i} onClick = {onClick}>
+    <div className = 'Food-List-Item-div' key = {i}><p className='Food-List-Item' key = {i}  onClick = {() => {compareName(restaurant.name); changeClicks(!click);}}>
     🍚   [{restaurant.name}]  👍🏻 <span className = 'Food-List-like'>{restaurant.like}</span>   {restaurant.star}  {restaurant.explanation}</p>
-    <div className = 'Food-List-Item-Image-div' key = {i} style = {{display : (clicks? "block" : "none")}}>
+    <div className = 'Food-List-Item-Image-div' key = {i} style = {{display : (name === restaurant.name && click ? "block" : "none")}}>
     <img className='Food-List-Item-Image' src = {restaurant.image} alt = {restaurant.name}/>
     <p>[ {restaurant.name} ]</p><p>📍 {restaurant.address}</p>🌐 <a href = {restaurant.kakao_map}> {restaurant.kakao_map}</a></div></div>)
    return <div className='Food-List-Container'>{list}</div>
